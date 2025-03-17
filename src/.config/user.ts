28e042fde585287d7db0f3080f3c155f8f1b77b5
SenLief <31976451+SenLief@users.noreply.github.com>
2025-03-18 00:30:13 +0800

    无趣的人生

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -2,6 +2,22 @@ import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
-  // site: { title: "講評世界" },
-  // seo: { twitter: "@moeyua13" },
+  site: { 
+    title: '浮生若梦',
+    subtitle: 'Dreaming',
+    author: 'JanVh',
+    description: '我曾想抓住每一寸光阴，可它总从指缝间流走，碎成月光。',
+    website: 'https://life.zjan.me',
+    pageSize: 7,
+    socialLinks: [
+      {
+        name: 'rss',
+        href: '/atom.xml',
+      },
+    ],
+   },
+  seo: { 
+    twitter: "@qqleap",
+    meta: ['文字', '感悟']
+  },
 }
